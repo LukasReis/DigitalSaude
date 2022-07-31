e866b71a338ddd6d100f08ae47b61c2f5b82469d
brendamaria <mariabrenda177@gmail.com>
2022-07-31 16:40:59 -0300

    atualizacao

--- DigitalSaude/src/main/java/managedbean/MedicosManagedBean.java
+++ DigitalSaude/src/main/java/managedbean/MedicosManagedBean.java
@@ -11,85 +11,80 @@ import javax.annotation.PostConstruct;
 
 import dao.DaoMedicos;
 import model.Medicos;
-
+import model.Pacientes;
 
 @ViewScoped
 @ManagedBean(name = "medicoManagedBean")
 public class MedicosManagedBean {
+
+	private Medicos medicos = new Medicos();
+	private List<Medicos> listMedicos;
+	private DaoMedicos<Medicos> daoMedicos = new DaoMedicos<Medicos>();
+	private String pesquisa;
+	private Medicos selectedMedicos;
+
+	@PostConstruct
+	public void init() {
+		listMedicos = daoMedicos.listar(Medicos.class);
+	}
+
+	// Metodos Acessores e Modificadores
+
+	public Medicos getMedicos() {
+		return medicos;
+	}
+
+	public void setMedicos(Medicos medicos) {
+		this.medicos = medicos;
+	}
+
+	public List<Medicos> getList() {
+		return listMedicos;
+	}
+
+	public String getPesquisa() {
+		return pesquisa;
+	}
+
+	public void setPesquisa(String pesquisa) {
+		this.pesquisa = pesquisa;
+	}
+
+	// Funcionalidades que serão acessadas via front
+
+	public String salvar() {
+		daoMedicos.salvar(medicos);
+		listMedicos.add(medicos);
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "INFORMAÇÃO: ", "Salvo com sucesso!"));
+		return "";
+	}
+
+	public String pesquisar() {
+		listMedicos = daoMedicos.pesquisarNome(pesquisa);
+		System.out.println(listMedicos);
+		return "";
+	}
+
+	public String novo() {
+		medicos = new Medicos();
+		return "";
+	}
 	
-		private Medicos medicos = new Medicos();
-		private List<Medicos> listMedicos = new ArrayList<Medicos>();
-		private DaoMedicos<Medicos> daoMedicos = new DaoMedicos<Medicos>();
-		private String pesquisa;
-		
-		@PostConstruct
-		public void init() {
-			listMedicos = daoMedicos.listar(Medicos.class);
-		}
+	public String deletar() {
+		daoMedicos.deletar(medicos);
+		listMedicos.remove(medicos); 
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "INFORMAÇÃO: ", "Excluído com sucesso!"));
+		return "";
+	}
+	
+	public Medicos getSelectedMedicos() {
+		return selectedMedicos;
+	}
 
-   		// Metodos Acessores e Modificadores 
+	public void setSelectedMedicos(Medicos selectedMedicos) {
+		this.selectedMedicos = selectedMedicos;
+	}
 
-		public Medicos getMedicos() { 
-			return medicos;
-		}
-
-		public void setMedicos(Medicos medicos) { 
-			this.medicos = medicos;
-		}
-
-		public List<Medicos> getList() {
-			return listMedicos;
-		}
-
-		public String getPesquisa() {
-			return pesquisa;
-		}
-
-		public void setPesquisa(String pesquisa) {
-			this.pesquisa = pesquisa;
-		}
-
-
-		// Funcionalidades que serão acessadas via front
-
-
-		public String salvar() {
-			daoMedicos.salvar(medicos);
-			listMedicos.add(medicos);
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_INFO, "INFORMAÇÃO: ", "Salvo com sucesso!"));
-			return "";
-		}
-
-		public String pesquisar() {
-			listMedicos = daoMedicos.pesquisarNome(pesquisa);
-			System.out.println(listMedicos);
-			return "";
-		} 
-
-
-		public String novo() {
-			medicos = new Medicos();
-			return "";
-		}
-
-
-//		public String deletar(){
-//			try{
-//			  daoMedicos.deltar(medicos);	
-//			  listMedicos.remove(medicos);
-//			 FacesContext.getCurrentInstance().addMessage(null,new FacesMessage
-//			(FacesMessage.SEVERITY_INFO, "Medico deletado com sucesso!"));
-//
-//		} catch (Exception e){
-//			if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
-//				FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
-//						"INFORMAÇÃO: ", "Você não pode excluir esse usuário antes de excluir os telefones!"));
-//			} else {
-//				e.printStackTrace();// joga no console outra exceção que não foi tratada
-//				}
-//
-//			}
-//      		 return "";
-//		}
 }
--- DigitalSaude/src/main/java/managedbean/PacienteManagedBean.java
+++ DigitalSaude/src/main/java/managedbean/PacienteManagedBean.java
@@ -15,66 +15,74 @@ import model.Pacientes;
 @ManagedBean(name = "pacienteManagedBean")
 public class PacienteManagedBean {
 
-		private Pacientes paciente = new Pacientes();
-		private List<Pacientes> list;
-		private DaoPaciente<Pacientes> daoPaciente = new DaoPaciente<Pacientes>();
-		private String pesquisa;
-		private Pacientes selectedPaciente;
-		
-		@PostConstruct
-		public void init() {
-			list = daoPaciente.listar(Pacientes.class);
-		}
-		
-		public Pacientes getPaciente() { 
-			return paciente;
-		}
+	private Pacientes paciente = new Pacientes();
+	private List<Pacientes> list;
+	private DaoPaciente<Pacientes> daoPaciente = new DaoPaciente<Pacientes>();
+	private String pesquisa;
+	private Pacientes selectedPaciente;
 
-		public void setPaciente(Pacientes paciente) { 
-			this.paciente = paciente;
-		}
+	@PostConstruct
+	public void init() {
+		list = daoPaciente.listar(Pacientes.class);
+	}
 
-		public List<Pacientes> getList() {
-			return list;
-		}
-		
-		public void setList(List<Pacientes> list) {
-	        this.list = list;
-	    }
+	public Pacientes getPaciente() {
+		return paciente;
+	}
 
-		public String getPesquisa() {
-			return pesquisa;
-		}
+	public void setPaciente(Pacientes paciente) {
+		this.paciente = paciente;
+	}
 
-		public void setPesquisa(String pesquisa) {
-			this.pesquisa = pesquisa;
-		}
-		
-		public Pacientes getSelectedPaciente() {
-	        return selectedPaciente;
-	    }
+	public List<Pacientes> getList() {
+		return list;
+	}
 
-	    public void setSelectedProduct(Pacientes selectedPaciente) {
-	        this.selectedPaciente = selectedPaciente;
-	    }
-		
-		// funcionalidade que serão acessadas via FRONT (Pág.XHTML)
-		public String salvar() {
-			daoPaciente.salvar(paciente);
-			list.add(paciente);
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_INFO, "INFORMAÇÃO: ", "Salvo com sucesso!"));
-			return "";
-		}
+	public void setList(List<Pacientes> list) {
+		this.list = list;
+	}
 
-		public String pesquisar() {
-			list = daoPaciente.pesquisarNome(pesquisa);
-			System.out.println(list);
-			return "";
-		}
+	public String getPesquisa() {
+		return pesquisa;
+	}
 
-		public void novo() {
-			System.out.println(this.selectedPaciente);
-		}
-		
+	public void setPesquisa(String pesquisa) {
+		this.pesquisa = pesquisa;
+	}
+
+	public Pacientes getSelectedPaciente() {
+		return selectedPaciente;
+	}
+
+	public void setSelectedPaciente(Pacientes selectedPaciente) {
+		this.selectedPaciente = selectedPaciente;
+	}
+
+	// funcionalidade que serão acessadas via FRONT (Pág.XHTML)
+	public String salvar() {
+		daoPaciente.salvar(paciente);
+		list.add(paciente);
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "INFORMAÇÃO: ", "Salvo com sucesso!"));
+		return "";
+	}
+
+	public String pesquisar() {
+		list = daoPaciente.pesquisarNome(pesquisa);
+		System.out.println(list);
+		return "";
+	}
+
+	public void novo() {
+		System.out.println(this.selectedPaciente);
+	}
+
+	public String deletar() {
+		daoPaciente.deletar(paciente);
+		list.remove(paciente); 
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "INFORMAÇÃO: ", "Excluído com sucesso!"));
+		return "";
+	}
+
 }
